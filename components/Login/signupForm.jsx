49a95c6ab15a8f68d48951d1d225ce4215c6bd77
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2024-07-26 19:39:11 +0530

    seprated textFeild as conponent

--- components/Login/signupForm.jsx
+++ components/Login/signupForm.jsx
@@ -3,6 +3,8 @@ import { projectContext } from "@/context/projectContext";
 import { checkUsernameAvalible } from "@/firebase/firebase.db";
 import Image from "next/image";
 import React, { useContext, useEffect, useState } from "react";
+import TextInput from "../formComponants/textInput";
+import Button from "../ui/Button";
 
 const SignupForm = () => {
     const { signUp } = useContext(projectContext);
@@ -33,7 +35,7 @@ const SignupForm = () => {
     };
 
     return (
-        <div className="w-full max-w-sm mx-auto overflow-hidden  rounded-lg shadow-md dark:bg-gray-800">
+        <div className="w-full max-w-sm mx-auto overflow-hidden border-2 border-neutral-600 border-dashed  rounded-lg shadow-md dark:bg-gray-800">
             <div className="px-6 py-4">
                 <div className="flex justify-center mx-auto">
                     <Image className="w-20" src={IMAGES.logo} alt="logo" />
@@ -52,25 +54,30 @@ const SignupForm = () => {
                         <p className="ml-2 text-red-300 text-xs">
                             Choose wisely you cant change username later
                         </p>
-                        <input
-                            className="block w-full px-4 py-2 mt-2 text-white placeholder-gray-400 bg-neutral-900 border-2 border-dashed rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
-                            type="text"
+                        
+
+                        <TextInput
                             required
                             placeholder="UserName"
                             onChange={(e) => setUsername(e.target.value)}
                             value={username}
                         />
                         {username && (
-                            <p className={`ml-2 mt-1 ${nameAvail ? "text-green-300" :"text-red-300"} text-xs`}>
+                            <p
+                                className={`ml-2 mt-1 ${
+                                    nameAvail
+                                        ? "text-green-300"
+                                        : "text-red-300"
+                                } text-xs`}
+                            >
                                 {username} {!nameAvail && "not "} is Avaliable
                             </p>
                         )}
                     </div>
 
                     <div className="w-full mt-4">
-                        <input
-                            className="block w-full px-4 py-2 mt-2 text-white placeholder-gray-400 bg-neutral-900 border-2 border-dashed rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
-                            type="password"
+                        <TextInput
+                            password
                             placeholder="Password"
                             required
                             onChange={(e) => setPass(e.target.value)}
@@ -78,15 +85,8 @@ const SignupForm = () => {
                         />
                     </div>
 
-                    <div className="flex items-center justify-between mt-4">
-                        <div></div>
-
-                        <button
-                            onClick={handleSubmit}
-                            className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
-                        >
-                            Sign In
-                        </button>
+                    <div className="flex items-center justify-end mt-4">
+                        <Button onClick={handleSubmit} lable="Sign In"/>
                     </div>
                 </form>
             </div>
