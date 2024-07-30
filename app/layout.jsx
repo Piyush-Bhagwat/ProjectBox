74959392885c0f90670d9925ac825126af52a493
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2024-07-30 19:33:00 +0530

    major changes: added route group updated user page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ProjectContext from "@/context/projectContext";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,11 +14,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <ProjectContext>
-                <body className={inter.className}>
-                    <Header />
-                    {children}
-                    <Footer />
-                </body>
+                <body className={inter.className}>{children}</body>
             </ProjectContext>
         </html>
     );
