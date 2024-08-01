91ebb660f386cb59ada0af92e74d4e9f1e82615e
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2024-08-01 16:47:21 +0530

    added manifest file

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,30 @@
-import { Inter, Work_Sans, Space_Mono } from "next/font/google";
+import { Space_Mono } from "next/font/google";
 import "./globals.css";
 import ProjectContext from "@/context/projectContext";
 
-const inter = Space_Mono({ subsets: ["latin"], weight: ["400", "700"], });
+const inter = Space_Mono({ subsets: ["latin"], weight: ["400", "700"] });
+
+const APP_NAME = "PWA App";
+const APP_DEFAULT_TITLE = "ProjectBox | Home";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Best PWA app in the world!";
 
 export const metadata = {
-    title: "ProjectBox | Home",
+    title: APP_DEFAULT_TITLE,
+    manifest: "/manifest.json",
     description:
         "ProjectBox is an innovative platform designed to help developers document, manage, and showcase their projects efficiently. It serves as a comprehensive repository where developers can store detailed information about their projects, making it easier to reference and share their work.",
+    appleWebApp: {
+        capable: true,
+        statusBarStyle: "default",
+        title: APP_DEFAULT_TITLE,
+        // startUpImage: [],
+    },
 };
+
+export const viewport = {
+    themeColor: "#404040",
+  };
 
 export default function RootLayout({ children }) {
     return (
--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
 const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: 'https',
-                hostname: '**',
-                port: '',
-                pathname: '**',
+                protocol: "https",
+                hostname: "**",
+                port: "",
+                pathname: "**",
             },
         ],
     },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
